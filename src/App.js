68bc4a906e0fd723e86fb517be0b2eda2730fdd7
Saraswat <saraswatmajumder@gmail.com>
2021-11-19 15:05:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,315 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import validator from "validator";
+import countryList from "./countrylist";
+import Newsfeed from "./newsfeed";
+import { cardcontext } from "./Context/cardviewcontext";
 function App() {
+  const [posts, setposts] = useState([]);
+  const [cardview, setcardview] = useState(true);
+  const [feedbackmodal, setfeedback] = useState(false);
+  const [newsmodal, setnewsback] = useState(false);
+  const [input, setinput] = useState("");
+  const [showcountrylist, setshowcountrylist] = useState(false);
+  const [country, setcountry] = useState("");
+  const [email, setemail] = useState("");
+  const [number, setnumber] = useState("");
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
+      const responseposts = res.data;
+      setposts(responseposts);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <cardcontext.Provider value={{ cardview, setcardview }}>
+      <div
+        className="App"
+        style={{ display: "flex", height: "100vh", width: "100vw" }}
+      >
+        <div
+          style={{
+            height: "92vh",
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "rgb(234,241,248)",
+            width: "20vw",
+            paddingTop: "8vh",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-start",
+              backgroundColor: "white",
+              padding: "1vw",
+              width: "15vw",
+              flexDirection: "column",
+            }}
+          >
+            <span style={{ fontWeight: "bold" }}> Hi Reader ,</span>
+
+            <div>Here's your news !</div>
+          </div>
+          {!feedbackmodal && (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "white",
+                padding: "1vw",
+                width: "15vw",
+                flexDirection: "column",
+                marginTop: "5vh",
+              }}
+            >
+              <div style={{ fontWeight: "bold", fontSize: "3vh" }}>
+                View Toggle
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  marginTop: "2vh",
+                  borderRadius: "10%",
+                  backgroundColor: "#D3D3D3",
+                }}
+              >
+                <div
+                  className={
+                    "bi bi-card-heading " +
+                    (cardview ? " toggleclicked" : " icons")
+                  }
+                  style={{
+                    fontSize: "5vh",
+                    padding: "2vh",
+                    borderTopLeftRadius: "10%",
+                    borderBottomLeftRadius: "10%",
+                  }}
+                  onClick={() => setcardview(true)}
+                ></div>
+                <div
+                  className={
+                    "bi bi-card-list icons" +
+                    (!cardview ? " toggleclicked" : " icons")
+                  }
+                  style={{
+                    fontSize: "5vh",
+                    padding: "2vh",
+                    borderTopRightRadius: "10%",
+                    borderBottomRightRadius: "10%",
+                  }}
+                  onClick={() => setcardview(false)}
+                ></div>
+              </div>
+            </div>
+          )}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "2vh",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "white",
+              marginTop: "5vh",
+              width: "15vw",
+            }}
+          >
+            <div style={{ fontSize: "3vh", fontWeight: "bold" }}>
+              Have a Feedback ?
+            </div>
+            <div
+              className={feedbackmodal ? "buttonclicked" : "button"}
+              style={{
+                fontSize: "2vh",
+                padding: "1vh",
+                fontWeight: "bold",
+                marginTop: "2vh",
+              }}
+              onClick={() => {
+                setfeedback(!feedbackmodal);
+              }}
+            >
+              We're Listening !
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundColor: "rgb(220,231,243)",
+          }}
+        >
+          <Newsfeed view={cardview} newsdata={posts} />
+        </div>
+        {feedbackmodal && (
+          <form
+            style={{
+              backgroundColor: "rgb(234,241,248)",
+              height: "94vh",
+              padding: "3vh",
+              position: "absolute",
+              marginLeft: "20vw",
+              display: "flex",
+              flexDirection: "column",
+              width: "50vw",
+            }}
+            autocomplete="false"
+          >
+            <h2>Thank you so much for taking the time !</h2>
+            <div style={{ marginTop: "1vh" }}>
+              Please provide the below details{" "}
+            </div>
+            <label for="name" style={{ fontWeight: "bold", marginTop: "3vh" }}>
+              First Name
+            </label>
+            <input
+              type="text"
+              style={{
+                backgroundColor: "white",
+                width: "25vw",
+                border: "none",
+                marginTop: "2vh",
+              }}
+            />
+            <label for="name" style={{ fontWeight: "bold", marginTop: "3vh" }}>
+              Last Name
+            </label>
+            <input
+              type="text"
+              style={{
+                backgroundColor: "white",
+                width: "25vw",
+                border: "none",
+                marginTop: "2vh",
+              }}
+            />
+            <br />
+            <label for="address" style={{ fontWeight: "bold" }}>
+              Address
+            </label>
+            <textarea
+              id="address"
+              rows="4"
+              columns="25"
+              style={{ marginBottom: "3vh", marginTop: "2vh", border: "none" }}
+            ></textarea>
+            <label
+              for="address"
+              style={{ fontWeight: "bold", marginBottom: "2vh" }}
+            >
+              Country
+            </label>
+            <input
+              type="search"
+              placeholder="Country"
+              style={{ width: "25vw", border: "none" }}
+              onChange={(event) => {
+                setinput(event.target.value);
+                setshowcountrylist(true);
+              }}
+              autocomplete="new-password"
+              value={input}
+            />
+            {showcountrylist && (
+              <div
+                style={{
+                  width: "24.5vw",
+                  paddingTop: "0.5vh",
+                  paddingLeft: "0.5vh",
+                  backgroundColor: "white",
+                  height: "30vh",
+                  overflow: "auto",
+                }}
+              >
+                {countryList
+                  .filter((val) => {
+                    if (input === "") {
+                      return val;
+                    } else if (
+                      val.toLowerCase().includes(input.toLowerCase())
+                    ) {
+                      return val;
+                    }
+                  })
+                  .map((val) => {
+                    return (
+                      <div
+                        style={{
+                          paddingTop: "0.5vh",
+                          paddingLeft: "0.5vh",
+                          marginTop: "0.2vh",
+                        }}
+                        className="listitem"
+                        onClick={() => {
+                          setcountry(val);
+                          setshowcountrylist(false);
+                        }}
+                      >
+                        {val}
+                      </div>
+                    );
+                  })}
+              </div>
+            )}
+            <label for="email" style={{ fontWeight: "bold", marginTop: "3vh" }}>
+              Email
+            </label>
+            <input
+              type="email"
+              onChange={(event) => {
+                setemail(event.target.value);
+              }}
+              style={{ width: "25vw", border: "none", marginTop: "2vh" }}
+            />
+            <div style={{ color: "red", fontSize: "1.5vh", marginTop: "0.5" }}>
+              {!validator.isEmail(email) && "Please Enter a valid mail"}
+            </div>
+            <label
+              for="number"
+              style={{ fontWeight: "bold", marginTop: "3vh" }}
+            >
+              Phone No.
+            </label>
+            <input
+              type="text"
+              onChange={(event) => {
+                setnumber(event.target.value);
+              }}
+              style={{ width: "25vw", border: "none", marginTop: "2vh" }}
+            />
+            <div style={{ color: "red", fontSize: "1.5vh", marginTop: "0.5" }}>
+              {!validator.isMobilePhone(number) &&
+                "Please Enter a valid number"}
+            </div>
+            <div
+              className="submit"
+              style={{
+                marginTop: "3vh",
+                padding: "1.5vh",
+                fontSize: "2vh",
+                width: "15vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                fontWeight: "bold",
+                border: "none",
+              }}
+              onClick={() => {
+                setfeedback(false);
+              }}
+            >
+              Submit
+            </div>
+          </form>
+        )}
+      </div>
+    </cardcontext.Provider>
   );
 }
 
---
+++ src/newsfeed.js
@@ -0,0 +1,252 @@
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import "./App.css";
+import { width } from "dom-helpers";
+import { cardcontext } from "./Context/cardviewcontext";
+
+const Newsfeed = (props) => {
+  const [uposts, setuposts] = useState(props.newsdata);
+  const [cview, setcview] = useState(props.view);
+  const { cardview, setcardview } = useContext(cardcontext);
+
+  const [fpage, setfpage] = useState(1);
+  const [cpage, setcpage] = useState(2);
+  const [npage, setnpage] = useState(3);
+  const [title, settitle] = useState(3);
+  const [desc, setdesc] = useState(3);
+  const [newsmodal, setnewsmodal] = useState(false);
+  const [activepage, setactivepage] = useState([]);
+  const [activeindex, setactiveindex] = useState(3);
+  const [limit, setlimit] = useState(0);
+  const deletenews = (id) => {
+    setactivepage(
+      activepage.filter((val) => {
+        return val.id !== id;
+      })
+    );
+  };
+  function setcred(t, d) {
+    settitle(t);
+    setdesc(d);
+  }
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => {
+        const responseposts = res.data;
+        setuposts(responseposts);
+      })
+      .then(() => {
+        setlimit(Math.ceil(uposts.length / 8));
+      });
+  }, []);
+
+  function handlenext() {
+    if (npage !== limit) {
+      setfpage(fpage + 1);
+      setcpage(cpage + 1);
+      setnpage(npage + 1);
+    }
+  }
+  function handleprev() {
+    if (fpage !== 1) {
+      setfpage(fpage - 1);
+      setcpage(cpage - 1);
+      setnpage(npage - 1);
+    }
+  }
+
+  let posts = [];
+  let i, j;
+  for (i = 0, j = 0; i < uposts.length + 8; i += 8, j++) {
+    posts[j] = uposts.slice(i, i + 8);
+  }
+  function handlenew(num) {
+    setactivepage(posts[num]);
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "80vw",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {newsmodal && (
+        <div
+          style={{
+            position: "absolute",
+            marginTop: "40vh",
+            marginLeft: "6vw",
+            padding: "1vw",
+            backgroundColor: "lightblue",
+            width: "50vw",
+            height: "30vh",
+            border: "10px",
+          }}
+        >
+          <div
+            style={{
+              marginLeft: "48vw",
+              fontSize: "2vw",
+              color: "lightblue",
+              fontWeight: "bold",
+              color: "black",
+            }}
+            onClick={() => {
+              setnewsmodal(!newsmodal);
+            }}
+          >
+            X
+          </div>
+          <b> {title}</b>
+          <br />
+          <br />
+          {desc}
+        </div>
+      )}
+      {cardview ? (
+        <div className="grid-display">
+          {activepage.map((val, key) => {
+            return (
+              <div
+                style={{
+                  width: "17vw",
+                  height: "35vh",
+                  display: "flex",
+                  flexDirection: "column",
+                  fontSize: "2vh",
+                  backgroundColor: "white",
+                }}
+              >
+                <div
+                  style={{
+                    marginLeft: "16vw",
+                    fontSize: "1vw",
+                    width: "17vw",
+                    marginTop: "1vh",
+                  }}
+                  onClick={() => deletenews(val.id)}
+                >
+                  X
+                </div>
+                <div
+                  style={{
+                    padding: "3vh",
+                    display: "flex",
+                    justifyContent: "center",
+                    width: "15vw",
+                    height: "30vh",
+                  }}
+                  onClick={() => {
+                    setnewsmodal(true);
+                    setcred(val.title, val.body);
+                  }}
+                >
+                  {val.title}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex-display">
+          {activepage.map((val, key) => {
+            return (
+              <div style={{ display: "flex", flexDirection: "row" }}>
+                <div
+                  style={{
+                    width: "60vw",
+                    backgroundColor: "white",
+                    marginBottom: "2vh",
+                    height: "4vh",
+                    padding: "2vh",
+                  }}
+                  onClick={() => {
+                    setnewsmodal(true);
+                    setcred(val.title, val.body);
+                  }}
+                >
+                  {val.title}
+                </div>
+                <div
+                  style={{
+                    fontWeight: "bold",
+                    marginLeft: "1vw",
+                    backgroundColor: "white",
+                    borderRadius: "50%",
+                    height: "2vw",
+                    width: "2vw",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginTop: "1vw",
+                  }}
+                  onClick={() => deletenews(val.id)}
+                >
+                  X
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "80vw",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <span
+          className="pagbutton"
+          style={{ fontSize: "4vh", fontWeight: "bolder" }}
+          onClick={handleprev}
+        >
+          &lt;
+        </span>
+        <span
+          className="navbutton"
+          onClick={() => {
+            handlenew(fpage - 1);
+          }}
+        >
+          {" "}
+          {fpage}{" "}
+        </span>
+        <span
+          className="navbutton"
+          onClick={() => {
+            handlenew(cpage - 1);
+          }}
+        >
+          {" "}
+          {cpage}{" "}
+        </span>
+        <span
+          className="navbutton"
+          onClick={() => {
+            handlenew(npage - 1);
+          }}
+        >
+          {" "}
+          {npage}{" "}
+        </span>
+        <span
+          className="pagbutton"
+          style={{ fontSize: "4vh", fontWeight: "bolder" }}
+          onClick={handlenext}
+        >
+          &gt;
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Newsfeed;
